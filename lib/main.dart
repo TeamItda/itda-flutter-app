63b55072c4e0b74b6c82d9d1cbc037b629622b93
seunghyun <ksh14628@naver.com>
2026-04-05 03:42:10 +0900

    feat: Firebase Auth 연동 (이메일 로그인/회원가입)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
-import 'package:firebase_core/firebase_core.dart';  // Firebase 연동
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();  // Firebase 연동
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCsDGv8oYY0TAncNzGFe7orsbjcsigE77U",
+      authDomain: "yeogiyo-59f27.firebaseapp.com",
+      projectId: "yeogiyo-59f27",
+      storageBucket: "yeogiyo-59f27.firebasestorage.app",
+      messagingSenderId: "292137139277",
+      appId: "1:292137139277:web:39c8cab9136825f01e3004",
+    ),
+  );
   runApp(const YeogiyoApp());
 }
